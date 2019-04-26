d81ac9cc2638f2c10998dc55c314141dcbcf55c4
drbarq <J.Tustin@gmail.com>
2019-04-26 15:38:51 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,10 +15,18 @@ class String
   end
 
   def count_sentences
-    # binding.pry
-    self.squeeze.count(".!?")
-# [\W]{3} REGEX 
-    #The test isnt explicit, in defining what counts as end of sentence puncuation
-    #RegEx is suppose to work as well.  Should I have counted the double "!!" as one or two?
+    self.gsub(/[!?.]{2,}/){|s|s[0]}.count(".?!")
+
+    # takes .self and uses gsub() function which replaces all matches of a string.  Using RegEx I identified instances within
+    # the string where ! or ? or . appear 2 or more times {2,} it then enumerates over those string instances locating the
+    # matches. It replaces s which is the identified repeating puncuation with s[0].  s[0] is the first character of the
+    # identified string.  this behavior effectivly reduces the repeating puncuation with one character.  Following the
+    # gsub actions is the count method which is counting the instances of . ? !.  It returns the number of instances of the new
+    # modified and subd string
+
+
+    # This is how I originally solved it.  The issue I had with the return is it mutated the original string.
+    # self.squeeze.count(".!?")
   end
+  
 end
