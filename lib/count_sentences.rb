8e3c47203504ab2e033dee3d504a13a05f31dd3c
drbarq <J.Tustin@gmail.com>
2019-04-26 14:47:25 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,9 @@ class String
   end
 
   def count_sentences
+    # binding.pry
     self.squeeze.count(".!?")
+# [\W]{3} REGEX 
     #The test isnt explicit, in defining what counts as end of sentence puncuation
     #RegEx is suppose to work as well.  Should I have counted the double "!!" as one or two?
   end
