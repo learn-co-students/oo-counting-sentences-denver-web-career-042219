302f31a3d3282d548c7a4c72010013fd82a6047e
drbarq <J.Tustin@gmail.com>
2019-04-26 14:13:24 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.squeeze.count(".!?")
+    #The test isnt explicit, in defining what counts as end of sentence puncuation
+    #RegEx is suppose to work as well.  Should I have counted the double "!!" as one or two?
   end
 end
